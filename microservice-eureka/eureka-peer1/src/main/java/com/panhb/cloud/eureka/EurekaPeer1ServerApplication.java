e9b0ed40a5646675cce73e900e7958cf76532ea7
panhongbo <hongbo.pan@secneo.com>
2017-06-13 13:02:57 +0800

    init

---
+++ microservice-eureka/eureka-peer1/src/main/java/com/panhb/cloud/eureka/EurekaPeer1ServerApplication.java
@@ -0,0 +1,15 @@
+package com.panhb.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaPeer1ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaPeer1ServerApplication.class, args);
+	}
+
+}
