846f9c2ec5a3d14e09586419c3ebebc4dac4e0cb
panhongbo <hongbo.pan@secneo.com>
2017-06-19 16:50:59 +0800

    init

---
+++ microservice-hystrix-dashboard/hystrix-dashboard/src/main/java/com/panhb/cloud/hystrix/dashboard/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.panhb.cloud.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+//		new SpringApplicationBuilder(HystrixDashboardApplication.class).web(true).run(args);
+	}
+
+}
